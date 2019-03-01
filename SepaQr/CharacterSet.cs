62f03596543c2e5e6da3fb8920197eaf720364c2
Wouter Huysentruit <wouter_huysentruit@hotmail.com>
2019-03-01 09:48:34 +0100

    Add CharacterSet

---
+++ SepaQr/CharacterSet.cs
@@ -0,0 +1,14 @@
+﻿namespace SepaQr
+{
+    public enum CharacterSet : byte
+    {
+        UTF8 = (byte)'1',
+        ISO8859_1 = (byte)'2',
+        ISO8859_2 = (byte)'3',
+        ISO8859_4 = (byte)'4',
+        ISO8859_5 = (byte)'5',
+        ISO8859_7 = (byte)'6',
+        ISO8859_10 = (byte)'7',
+        ISO8859_15 = (byte)'8'
+    }
+}
