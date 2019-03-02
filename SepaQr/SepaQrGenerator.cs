e553907282e60848b89d07a6ac694b60bdea1d29
Wouter Huysentruit <wouter_huysentruit@hotmail.com>
2019-03-02 09:49:19 +0100

    Add defaults for SepaQrGenerator

---
+++ SepaQr/SepaQrGenerator.cs
@@ -0,0 +1,13 @@
+﻿namespace SepaQr
+{
+    public sealed class SepaQrCode
+    {
+        public const string DefaultServiceTag = "BCD";
+
+        public const string DefaultVersion = "002";
+
+        public const CharacterSet DefaultCharacterSet = CharacterSet.UTF8;
+
+        public const string DefaultIdentificationCode = "SCT";
+    }
+}
